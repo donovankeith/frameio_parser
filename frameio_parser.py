269cbc0f21f6886477bec8b07006d01ac753b33b
Donovan Keith <donovanskeith@gmail.com>
2015-07-30 17:27:05 -0700

    Read in export.txt

--- frameio_parser.py
+++ frameio_parser.py
@@ -1,5 +1,13 @@
+"""Frame.io Comment Export Parser
+(Eventually will...) Takes a "export.txt" in the same directory and converts it into a usable format for pasting into GoogleSheets.
+"""
+
 def main():
-    print "main"
+    with open('export.txt') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            print line
 
 if __name__ == "__main__":
     main()
